0d024583b30acfb5cfc23fa4252f0b8fe707abda
Rainer Friederich <rainerfriederich@wearewondrous.com>
2021-10-19 08:03:17 +0200

    add support for relatable query based on https://github.com/Benjacho/belongs-to-many-field-nova/pull/46/files

--- src/BelongsToManyField.php
+++ src/BelongsToManyField.php
@@ -6,254 +6,333 @@ use Benjacho\BelongsToManyField\Rules\ArrayRules;
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Fields\ResourceRelationshipGuesser;
 use Laravel\Nova\Http\Requests\NovaRequest;
+use Laravel\Nova\Fields\FormatsRelatableDisplayValues;
+use Laravel\Nova\TrashedStatus;
+use Illuminate\Support\Str;
 
-class BelongsToManyField extends Field
-{
-    /**
-     * The callback to be used for the field's options.
-     *
-     * @var array|callable
-     */
-    private $optionsCallback;
+class BelongsToManyField extends Field {
 
-    public $showOnIndex = true;
-    public $showOnDetail = true;
-    public $isAction = false;
-    public $selectAll = false;
-    public $messageSelectAll = 'Select All';
-    public $height = '350px';
-    public $viewable = true;
-    public $showAsList = false;
-    public $pivotData = [];
-    /**
-     * The field's component.
-     *
-     * @var string
-     */
-    public $component = 'BelongsToManyField';
-    public $relationModel;
-    public $label = null;
-    public $trackBy = "id";
+  use FormatsRelatableDisplayValues;
 
-    /**
-     * Create a new field.
-     *
-     * @param string $name
-     * @param string|null $attribute
-     * @param string|null $resource
-     *
-     * @return void
-     */
-    public function __construct($name, $attribute = null, $resource = null)
-    {
-        parent::__construct($name, $attribute);
-        $resource = $resource ?? ResourceRelationshipGuesser::guessResource($name);
+  /**
+   * The column that should be displayed for the field.
+   *
+   * @var \Closure
+   */
+  public $display;
 
-        $this->resource = $resource;
+  /**
+   * The callback to be used for the field's options.
+   *
+   * @var array|callable
+   */
+  private $optionsCallback;
 
-        if ($this->label === null) {
-            $this->optionsLabel(($resource)::$title ?? 'name');
-        }
+  public $showOnIndex = TRUE;
 
-        $this->resourceClass = $resource;
-        $this->resourceName = $resource::uriKey();
-        $this->manyToManyRelationship = $this->attribute;
+  public $showOnDetail = TRUE;
 
-        $this->fillUsing(function ($request, $model, $attribute, $requestAttribute) use ($resource) {
-            if (is_subclass_of($model, 'Illuminate\Database\Eloquent\Model')) {
-                $model::saved(function ($model) use ($attribute, $request) {
-                    $inp = json_decode($request->input($attribute), true);
+  public $isAction = FALSE;
 
-                    if ($inp !== null) {
-                        $values = array_column($inp, 'id');
-                    } else {
-                        $values = [];
-                    }
+  public $selectAll = FALSE;
 
-                    if (!empty($this->pivot())) {
-                        $values = array_fill_keys($values, $this->pivot());
-                    }
+  public $messageSelectAll = 'Select All';
 
-                    $model->$attribute()->sync(
-                        $values
-                    );
-                });
-                $request->except($attribute);
-            }
+  public $height = '350px';
+
+  public $viewable = TRUE;
+
+  public $showAsList = FALSE;
+
+  public $pivotData = [];
+
+  /**
+   * The field's component.
+   *
+   * @var string
+   */
+  public $component = 'BelongsToManyField';
+
+  public $relationModel;
+
+  public $label = NULL;
+
+  public $trackBy = "id";
+
+  /**
+   * Create a new field.
+   *
+   * @param string $name
+   * @param string|null $attribute
+   * @param string|null $resource
+   *
+   * @return void
+   */
+  public function __construct($name, $attribute = NULL, $resource = NULL) {
+    parent::__construct($name, $attribute);
+    $resource = $resource ?? ResourceRelationshipGuesser::guessResource($name);
+
+    $this->resource = $resource;
+
+    if ($this->label === NULL) {
+      $this->optionsLabel(($resource)::$title ?? 'name');
+    }
+
+    $this->resourceClass = $resource;
+    $this->resourceName = $resource::uriKey();
+    $this->manyToManyRelationship = $this->attribute;
+
+    $this->fillUsing(function ($request, $model, $attribute, $requestAttribute) use ($resource) {
+      if (is_subclass_of($model, 'Illuminate\Database\Eloquent\Model')) {
+        $model::saved(function ($model) use ($attribute, $request) {
+          $inp = json_decode($request->input($attribute), TRUE);
+
+          if ($inp !== NULL) {
+            $values = array_column($inp, 'id');
+          }
+          else {
+            $values = [];
+          }
+
+          if (!empty($this->pivot())) {
+            $values = array_fill_keys($values, $this->pivot());
+          }
+
+          $model->$attribute()->sync(
+            $values
+          );
         });
-        $this->localize();
+        $request->except($attribute);
+      }
+    });
+    $this->localize();
+  }
+
+  public function optionsLabel(string $optionsLabel) {
+    $this->label = $optionsLabel;
+
+    return $this->withMeta(['optionsLabel' => $this->label]);
+  }
+
+  public function trackBy(string $trackBy) {
+    $this->trackBy = $trackBy;
+    return $this->withMeta(['trackBy' => $this->trackBy]);
+  }
+
+  public function options($options = []) {
+    $this->optionsCallback = $options;
+
+    return $this;
+  }
+
+  public function relationModel($model) {
+    $this->relationModel = $model;
+
+    return $this;
+  }
+
+  public function isAction($isAction = TRUE) {
+    $this->isAction = $isAction;
+
+    return $this->withMeta(['height' => $this->height]);
+  }
+
+  public function canSelectAll($messageSelectAll = 'Select All', $selectAll = TRUE) {
+    $this->selectAll = $selectAll;
+    $this->messageSelectAll = $messageSelectAll;
+
+    return $this->withMeta(['selectAll' => $this->selectAll, 'messageSelectAll' => $this->messageSelectAll]);
+  }
+
+  public function showAsListInDetail($showAsList = TRUE) {
+    $this->showAsList = $showAsList;
+
+    return $this->withMeta(['showAsList' => $this->showAsList]);
+  }
+
+  public function viewable($viewable = TRUE) {
+    $this->viewable = $viewable;
+
+    return $this;
+  }
+
+  public function setMultiselectProps($props) {
+    return $this->withMeta(['multiselectOptions' => $props]);
+  }
+
+  public function setMultiselectSlots($slots) {
+    return $this->withMeta(['multiselectSlots' => $slots]);
+  }
+
+  public function dependsOn($dependsOnField, $tableKey) {
+    return $this->withMeta([
+      'dependsOn' => $dependsOnField,
+      'dependsOnKey' => $tableKey,
+    ]);
+  }
+
+  public function rules($rules) {
+    $rules = ($rules instanceof Rule || is_string($rules)) ? func_get_args() : $rules;
+    $this->rules = [new ArrayRules($rules)];
+
+    return $this;
+  }
+
+  public function resolve($resource, $attribute = NULL) {
+    if ($this->isAction) {
+      parent::resolve($resource, $attribute);
     }
+    else {
+      parent::resolve($resource, $attribute);
+      $value = json_decode($resource->{$this->attribute});
 
-    public function optionsLabel(string $optionsLabel)
-    {
-        $this->label = $optionsLabel;
-
-        return $this->withMeta(['optionsLabel' => $this->label]);
+      if ($value) {
+        $this->value = $value;
+      }
     }
+  }
 
-    public function trackBy(string $trackBy)
-    {
-        $this->trackBy = $trackBy;
-        return $this->withMeta(['trackBy' => $this->trackBy]);
+  public function jsonSerialize() {
+    $this->resolveOptions();
+
+    return array_merge([
+      'attribute' => $this->attribute,
+      'component' => $this->component(),
+      'helpText' => $this->getHelpText(),
+      'indexName' => $this->name,
+      'name' => $this->name,
+      'nullable' => $this->nullable,
+      'optionsLabel' => $this->label,
+      'trackBy' => $this->trackBy,
+      'panel' => $this->panel,
+      'prefixComponent' => TRUE,
+      'readonly' => $this->isReadonly(app(NovaRequest::class)),
+      'required' => $this->isRequired(app(NovaRequest::class)),
+      'resourceNameRelationship' => $this->resourceName,
+      'sortable' => $this->sortable,
+      'sortableUriKey' => $this->sortableUriKey(),
+      'stacked' => $this->stacked,
+      'textAlign' => $this->textAlign,
+      'value' => $this->value,
+      'viewable' => $this->viewable,
+      'validationKey' => $this->validationKey(),
+    ], $this->meta());
+  }
+
+  /**
+   * Build an attachable query for the field.
+   *
+   * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+   * @param bool $withTrashed
+   *
+   * @return \Illuminate\Database\Eloquent\Builder
+   */
+  public function buildAttachableQuery(NovaRequest $request, $withTrashed = FALSE) {
+    $model = forward_static_call([$resourceClass = $this->resourceClass, 'newModel']);
+
+    $query = $request->first === 'true'
+      ? $model->newQueryWithoutScopes()->whereKey($request->current)
+      : $resourceClass::buildIndexQuery(
+        $request,
+        $model->newQuery(),
+        $request->search,
+        [],
+        [],
+        TrashedStatus::fromBoolean($withTrashed)
+      );
+
+    return $query->tap(function ($query) use ($request, $model) {
+      forward_static_call($this->attachableQueryCallable($request, $model), $request, $query);
+    });
+  }
+
+  /**
+   * Get the attachable query method name.
+   *
+   * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+   * @param \Illuminate\Database\Eloquent\Model $model
+   *
+   * @return array
+   */
+  protected function attachableQueryCallable(NovaRequest $request, $model) {
+    return ($method = $this->attachableQueryMethod($request, $model))
+      ? [$request->resource(), $method]
+      : [$this->resourceClass, 'relatableQuery'];
+  }
+
+  /**
+   * Get the attachable query method name.
+   *
+   * @param \Laravel\Nova\Http\Requests\NovaRequest $request
+   * @param \Illuminate\Database\Eloquent\Model $model
+   *
+   * @return string
+   */
+  protected function attachableQueryMethod(NovaRequest $request, $model) {
+    $method = 'relatable' . Str::plural(class_basename($model));
+
+    if (method_exists($request->resource(), $method)) {
+      return $method;
     }
+  }
 
-    public function options($options = [])
-    {
-        $this->optionsCallback = $options;
 
-        return $this;
+  /**
+   * Formats the display values to show in the different views.
+   *
+   * @param mixed $resource
+   *
+   * @return array
+   */
+  public function formatDisplayValues($resource, $optionsLabel) {
+    return [
+      'id' => $resource->getKey(),
+      $optionsLabel => $resource->title(),
+      'value' => $this->formatDisplayValue($resource),
+    ];
+  }
+
+  public function pivot() {
+    return $this->pivotData;
+  }
+
+  public function setPivot(array $attributes) {
+    $this->pivotData = $attributes;
+
+    return $this;
+  }
+
+  protected function localize() {
+    $this->setMultiselectProps([
+      'selectLabel' => __('belongs-to-many-field-nova::vue-multiselect.select_label'),
+      'selectGroupLabel' => __('belongs-to-many-field-nova::vue-multiselect.select_group_label'),
+      'selectedLabel' => __('belongs-to-many-field-nova::vue-multiselect.selected_label'),
+      'deselectLabel' => __('belongs-to-many-field-nova::vue-multiselect.deselect_label'),
+      'deselectGroupLabel' => __('belongs-to-many-field-nova::vue-multiselect.deselect_group_label'),
+    ]);
+
+    $this->setMultiselectSlots([
+      'noOptions' => $this->getNoOptionsSlot(),
+      'noResult' => $this->getNoResultSlot(),
+    ]);
+  }
+
+  protected function getNoOptionsSlot() {
+    return __('belongs-to-many-field-nova::vue-multiselect.no_options');
+  }
+
+  protected function getNoResultSlot() {
+    return __('belongs-to-many-field-nova::vue-multiselect.no_result');
+  }
+
+  private function resolveOptions(): void {
+    if (isset($this->optionsCallback)) {
+      if (is_callable($this->optionsCallback)) {
+        $this->withMeta(['options' => call_user_func($this->optionsCallback)]);
+      }
+      else {
+        $this->withMeta(['options' => collect($this->optionsCallback)]);
+      }
     }
-
-    public function relationModel($model)
-    {
-        $this->relationModel = $model;
-
-        return $this;
-    }
-
-    public function isAction($isAction = true)
-    {
-        $this->isAction = $isAction;
-
-        return $this->withMeta(['height' => $this->height]);
-    }
-
-    public function canSelectAll($messageSelectAll = 'Select All', $selectAll = true)
-    {
-        $this->selectAll = $selectAll;
-        $this->messageSelectAll = $messageSelectAll;
-
-        return $this->withMeta(['selectAll' => $this->selectAll, 'messageSelectAll' => $this->messageSelectAll]);
-    }
-
-    public function showAsListInDetail($showAsList = true)
-    {
-        $this->showAsList = $showAsList;
-
-        return $this->withMeta(['showAsList' => $this->showAsList]);
-    }
-
-    public function viewable($viewable = true)
-    {
-        $this->viewable = $viewable;
-
-        return $this;
-    }
-
-    public function setMultiselectProps($props)
-    {
-        return $this->withMeta(['multiselectOptions' => $props]);
-    }
-
-    public function setMultiselectSlots($slots)
-    {
-        return $this->withMeta(['multiselectSlots' => $slots]);
-    }
-
-    public function dependsOn($dependsOnField, $tableKey)
-    {
-        return $this->withMeta([
-            'dependsOn' => $dependsOnField,
-            'dependsOnKey' => $tableKey,
-        ]);
-    }
-
-    public function rules($rules)
-    {
-        $rules = ($rules instanceof Rule || is_string($rules)) ? func_get_args() : $rules;
-        $this->rules = [new ArrayRules($rules)];
-
-        return $this;
-    }
-
-    public function resolve($resource, $attribute = null)
-    {
-        if ($this->isAction) {
-            parent::resolve($resource, $attribute);
-        } else {
-            parent::resolve($resource, $attribute);
-            $value = json_decode($resource->{$this->attribute});
-
-            if ($value) {
-                $this->value = $value;
-            }
-        }
-    }
-
-    public function jsonSerialize()
-    {
-        $this->resolveOptions();
-
-        return array_merge([
-            'attribute' => $this->attribute,
-            'component' => $this->component(),
-            'helpText' => $this->getHelpText(),
-            'indexName' => $this->name,
-            'name' => $this->name,
-            'nullable' => $this->nullable,
-            'optionsLabel' => $this->label,
-            'trackBy' => $this->trackBy,
-            'panel' => $this->panel,
-            'prefixComponent' => true,
-            'readonly' => $this->isReadonly(app(NovaRequest::class)),
-            'required' => $this->isRequired(app(NovaRequest::class)),
-            'resourceNameRelationship' => $this->resourceName,
-            'sortable' => $this->sortable,
-            'sortableUriKey' => $this->sortableUriKey(),
-            'stacked' => $this->stacked,
-            'textAlign' => $this->textAlign,
-            'value' => $this->value,
-            'viewable' => $this->viewable,
-            'validationKey' => $this->validationKey(),
-        ], $this->meta());
-    }
-
-    public function pivot()
-    {
-        return $this->pivotData;
-    }
-
-    public function setPivot(array $attributes)
-    {
-        $this->pivotData = $attributes;
-
-        return $this;
-    }
-
-    protected function localize()
-    {
-        $this->setMultiselectProps([
-            'selectLabel' => __('belongs-to-many-field-nova::vue-multiselect.select_label'),
-            'selectGroupLabel' => __('belongs-to-many-field-nova::vue-multiselect.select_group_label'),
-            'selectedLabel' => __('belongs-to-many-field-nova::vue-multiselect.selected_label'),
-            'deselectLabel' => __('belongs-to-many-field-nova::vue-multiselect.deselect_label'),
-            'deselectGroupLabel' => __('belongs-to-many-field-nova::vue-multiselect.deselect_group_label'),
-        ]);
-
-        $this->setMultiselectSlots([
-            'noOptions' => $this->getNoOptionsSlot(),
-            'noResult' => $this->getNoResultSlot()
-        ]);
-    }
-
-    protected function getNoOptionsSlot()
-    {
-        return __('belongs-to-many-field-nova::vue-multiselect.no_options');
-    }
-
-    protected function getNoResultSlot()
-    {
-        return __('belongs-to-many-field-nova::vue-multiselect.no_result');
-    }
-
-    private function resolveOptions(): void
-    {
-        if (isset($this->optionsCallback)) {
-            if (is_callable($this->optionsCallback)) {
-                $this->withMeta(['options' => call_user_func($this->optionsCallback)]);
-            } else {
-                $this->withMeta(['options' => collect($this->optionsCallback)]);
-            }
-        }
-    }
+  }
 }
--- src/Http/Controllers/ResourceController.php
+++ src/Http/Controllers/ResourceController.php
@@ -6,42 +6,39 @@ use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Fields\FieldCollection;
 use Laravel\Nova\Http\Requests\NovaRequest;
 
-class ResourceController
-{
-    public function index(NovaRequest $request, $parent, $relationship, $optionsLabel, $dependsOnValue = null, $dependsOnKey = null)
-    {
-        $resourceClass = $request->newResource();
-        $fieldCollection = $resourceClass->availableFields($request);
-        $field = $fieldCollection->reduce(function ($carry, $item) {
-            if ($item->component === 'nova-dependency-container') {
-                $carry = $carry->merge($item->meta()['fields']);
-            } else {
-                $carry->push($item);
-            }
-            return $carry;
-        }, FieldCollection::make([]))->where('component', 'BelongsToManyField')
-            ->where('attribute', $relationship)
-            ->first();
-        $query = $field->resourceClass::newModel();
+class ResourceController {
 
-        $queryResult = $field->resourceClass::relatableQuery($request, $query);
+  public function index(NovaRequest $request, $parent, $relationship, $optionsLabel, $dependsOnValue = NULL, $dependsOnKey = NULL) {
+    $resourceClass = $request->newResource();
+    $fieldCollection = $resourceClass->availableFields($request);
+    $field = $fieldCollection->reduce(function ($carry, $item) {
+      if ($item->component === 'nova-dependency-container') {
+        $carry = $carry->merge($item->meta()['fields']);
+      }
+      else {
+        $carry->push($item);
+      }
+      return $carry;
+    }, FieldCollection::make([]))
+      ->where('component', 'BelongsToManyField')
+      ->where('attribute', $relationship)
+      ->first();
 
-        if ($dependsOnValue) {
-            $queryResult = $queryResult->where($dependsOnKey, $dependsOnValue);
-        }
+    $query = $field->buildAttachableQuery($request, FALSE);
 
-        return $queryResult->get()
-            ->mapInto($field->resourceClass)
-            ->filter(function ($resource) use ($request, $field) {
-                return $request->newResource()->authorizedToAttach($request, $resource->resource);
-            })->map(function ($resource) use ($optionsLabel) {
-                return [
-                    'id' => $resource->id,
-                    $optionsLabel => $resource->title(),
-                    'value' => $resource->getKey(),
-                ];
-            })
-            ->sortBy($optionsLabel)
-            ->values();
+    if ($request->dependsOnValue) {
+      $query = $query->where(
+        $request->dependsOnKey,
+        $request->dependsOnValue
+      );
     }
+
+    return $query->get()
+      ->mapInto($field->resourceClass)
+      ->filter(function ($resource) use ($request, $field) {
+        return $request->newResource()->authorizedToAttach($request, $resource->resource);
+      })->map(function ($resource) use ($field, $optionsLabel) {
+        return $field->formatDisplayValues($resource, $optionsLabel);
+      })->sortBy($optionsLabel)->values();
+  }
 }
